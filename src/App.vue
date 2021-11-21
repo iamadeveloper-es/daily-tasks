c981d02f1722790029b796e608d1232c4e202617
Daniel Verdu <iamadeveloper.es@gmail.com>
2021-11-21 09:29:53 +0100

    project initialized and configured

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <dashboard></dashboard>
+  </div>
+</template>
+<script>
+import Dashboard from "@/components/templates/dashboard"
+export default{
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/static/scss/main";
+</style>
